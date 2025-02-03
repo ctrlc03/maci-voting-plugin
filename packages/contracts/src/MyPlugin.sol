fc8a5d6d236a6598048a7ee18800f3a8ab87565c
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-02-03 15:36:16 +0500

    feat: implement MACI voting plugin contract

--- packages/contracts/src/MyPlugin.sol
+++ packages/contracts/src/MyPlugin.sol
@@ -4,38 +4,158 @@ pragma solidity ^0.8.8;
 import {PluginUUPSUpgradeable} from "@aragon/osx-commons-contracts/src/plugin/PluginUUPSUpgradeable.sol";
 import {IDAO} from "@aragon/osx-commons-contracts/src/dao/IDAO.sol";
 
+import {IVotesUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
+import {SafeCastUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import {IMACI} from "@maci-contracts/maci-contracts/src/interfaces/IMACI.sol";
+
 /// @title MyPlugin
 /// @dev Release 1, Build 1
-contract MyPlugin is PluginUUPSUpgradeable {
+contract MaciVoting is PluginUUPSUpgradeable {
+    using SafeCastUpgradeable for uint256;
+
+    /// @notice The [ERC-165](https://eips.ethereum.org/EIPS/eip-165) interface ID of the contract.
+    bytes4 internal constant MACI_VOTING_INTERFACE_ID =
+        this.initialize.selector ^ this.getVotingToken.selector;
+
+    /// @notice An [OpenZeppelin `Votes`](https://docs.openzeppelin.com/contracts/4.x/api/governance#Votes) compatible contract referencing the token being used for voting.
+    IVotesUpgradeable private votingToken;
+
     /// @notice The ID of the permission required to call the `storeNumber` function.
-    bytes32 public constant STORE_PERMISSION_ID = keccak256("STORE_PERMISSION");
+    bytes32 public constant CREATE_PROPOSAL_PERMISSION_ID = keccak256("CREATE_PROPOSAL_PERMISSION");
 
-    uint256 public number; // added in build 1
-
-    /// @notice Emitted when a number is stored.
-    /// @param number The number.
-    event NumberStored(uint256 number);
+    IMACI public maci;
 
     /// @notice Disables the initializers on the implementation contract to prevent it from being left uninitialized.
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
+
     }
 
     /// @notice Initializes the plugin when build 1 is installed.
     /// @param _number The number to be stored.
-    function initialize(IDAO _dao, uint256 _number) external initializer {
+    function initialize(IDAO _dao, address _maci) external initializer {
         __PluginUUPSUpgradeable_init(_dao);
-        number = _number;
 
-        emit NumberStored({number: _number});
+        maci = IMACI(_maci);
     }
 
-    /// @notice Stores a new number to storage. Caller needs STORE_PERMISSION.
-    /// @param _number The number to be stored.
-    function storeNumber(uint256 _number) external auth(STORE_PERMISSION_ID) {
-        number = _number;
+    /// @inheritdoc MajorityVotingBase
+    function createProposal(
+        bytes calldata _metadata,
+        IDAO.Action[] calldata _actions,
+        uint256 _allowFailureMap,
+        uint64 _startDate,
+        uint64 _endDate,
+        VoteOption _voteOption,        
+    ) external override returns (uint256 proposalId) {
+        // Check that either `_msgSender` owns enough tokens or has enough voting power from being a delegatee.
+        {
+            uint256 minProposerVotingPower_ = minProposerVotingPower();
 
-        emit NumberStored({number: _number});
+            if (minProposerVotingPower_ != 0) {
+                // Because of the checks in `TokenVotingSetup`, we can assume that `votingToken` is an [ERC-20](https://eips.ethereum.org/EIPS/eip-20) token.
+                if (
+                    votingToken.getVotes(_msgSender()) < minProposerVotingPower_ &&
+                    IERC20Upgradeable(address(votingToken)).balanceOf(_msgSender()) <
+                    minProposerVotingPower_
+                ) {
+                    revert ProposalCreationForbidden(_msgSender());
+                }
+            }
+        }
+
+        uint256 snapshotBlock;
+        unchecked {
+            snapshotBlock = block.number - 1; // The snapshot block must be mined already to protect the transaction against backrunning transactions causing census changes.
+        }
+
+        uint256 totalVotingPower_ = totalVotingPower(snapshotBlock);
+
+        if (totalVotingPower_ == 0) {
+            revert NoVotingPower();
+        }
+
+        (_startDate, _endDate) = _validateProposalDates(_startDate, _endDate);
+
+        proposalId = _createProposal({
+            _creator: _msgSender(),
+            _metadata: _metadata,
+            _startDate: _startDate,
+            _endDate: _endDate,
+            _actions: _actions,
+            _allowFailureMap: _allowFailureMap
+        });
+
+        // Store proposal related information
+        Proposal storage proposal_ = proposals[proposalId];
+
+        proposal_.parameters.startDate = _startDate;
+        proposal_.parameters.endDate = _endDate;
+        proposal_.parameters.snapshotBlock = snapshotBlock.toUint64();
+        proposal_.parameters.votingMode = votingMode();
+        proposal_.parameters.supportThreshold = supportThreshold();
+        proposal_.parameters.minVotingPower = _applyRatioCeiled(
+            totalVotingPower_,
+            minParticipation()
+        );
+
+        // Reduce costs
+        if (_allowFailureMap != 0) {
+            proposal_.allowFailureMap = _allowFailureMap;
+        }
+
+        for (uint256 i; i < _actions.length; ) {
+            proposal_.actions.push(_actions[i]);
+            unchecked {
+                ++i;
+            }
+        }
+
+        IMACI(maci).deployPoll({
+            duration: _endDate - _startDate,
+            treeDepths: {
+                intStateTreeDepth: 2,
+                voteOptionTreeDepth: 1
+            },
+            messageBatchSize: 20,
+            coordinatorPubKey: {
+                x: 1,
+                y: 1
+            },
+            verifier: address(0),
+            vkRegistry: address(0),
+            mode: 1,
+            gatekeeper: address(this),
+            initialVoiceCreditProxy: address(this),
+            relayers: new address[](address(0)),
+        });
+    }
+    
+    /// @inheritdoc MajorityVotingBase
+    function _vote(
+        uint256 _proposalId,
+        VoteOption _voteOption,
+        address _voter,
+        bool _tryEarlyExecution
+    ) internal override {
+        Proposal storage proposal_ = proposals[_proposalId];
+
+       
+    }
+
+    /**
+     * Gatekeeper function to register users
+     */
+    function register(address _address, bytes memory _data) public {
+
+    }
+
+    /**
+     * Function to get the voice credits of a user based on their token balances
+     */
+    function getVoiceCredits(address _address, bytes memory _data) public view returns (uint256) {
+        uint256 votingPower = votingToken.getPastVotes()
     }
 }
